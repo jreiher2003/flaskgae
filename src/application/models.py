77177da6bc40bac2de6826b2f4cd1c84ebd172f7
Jeff Reiher <jeffreiher@gmail.com>
2015-04-27 09:16:25 -0400

    configured models

--- src/application/models.py
+++ src/application/models.py
@@ -11,7 +11,7 @@ from google.appengine.ext import ndb
 
 class ExampleModel(ndb.Model):
     """Example Model"""
-    example_name = ndb.StringProperty(required=True)
-    example_description = ndb.TextProperty(required=True)
-    added_by = ndb.UserProperty()
+    name = ndb.StringProperty(required=True)
+    email = ndb.TextProperty(required=True)
+    password = ndb.StringProperty(required=True)
     timestamp = ndb.DateTimeProperty(auto_now_add=True)
